6466e40dfd0f0a66eacac614ca39a1f263f3fab5
Kadir <k1d12r04@gmail.com>
2023-06-23 15:32:30 +0300

    Include type definitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Banner from '@/components/Banner';
 import Header from '@/components/Header';
 import requests from '@/utils/requests';
-import type { NextPage } from 'next';
 import Head from 'next/head';
 
-const Home: NextPage = () => {
+const Home = ({ netflixOriginals }) => {
+  console.log(netflixOriginals);
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ">
       <Head>
