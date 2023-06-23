fce30e86c6db815789b08170e84b3c15bfd76d63
Kadir <k1d12r04@gmail.com>
2023-06-23 15:24:15 +0300

    Structure server side rendering process

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Banner from '@/components/Banner';
 import Header from '@/components/Header';
+import requests from '@/utils/requests';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
@@ -30,3 +31,37 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then(res => res.json()),
+    fetch(requests.fetchTrending).then(res => res.json()),
+    fetch(requests.fetchTopRated).then(res => res.json()),
+    fetch(requests.fetchActionMovies).then(res => res.json()),
+    fetch(requests.fetchComedyMovies).then(res => res.json()),
+    fetch(requests.fetchHorrorMovies).then(res => res.json()),
+    fetch(requests.fetchRomanceMovies).then(res => res.json()),
+    fetch(requests.fetchDocumentaries).then(res => res.json()),
+  ]);
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  };
+};
