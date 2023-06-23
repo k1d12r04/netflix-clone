aaf67fdf2be1d33fb7695889083ca096c28ef560
Kadir <k1d12r04@gmail.com>
2023-06-23 15:39:50 +0300

    Define types for incoming objects from api request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import Banner from '@/components/Banner';
 import Header from '@/components/Header';
+import { Movie } from '@/typing';
 import requests from '@/utils/requests';
 import Head from 'next/head';
 
-const Home = ({ netflixOriginals }) => {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
+
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries,
+}: Props) => {
   console.log(netflixOriginals);
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ">
