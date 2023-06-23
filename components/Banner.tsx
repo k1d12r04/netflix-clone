14a4123aafc0d83800a826e4ade80589edfbb2b2
Kadir <k1d12r04@gmail.com>
2023-06-23 17:33:53 +0300

    Show random banner image on every load

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,8 @@
 import { Movie } from '@/typing';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { baseUrl } from '@/constants/movie';
+
 interface Props {
   netflixOriginals: Movie[];
 }
@@ -16,7 +18,24 @@ const Banner = ({ netflixOriginals }: Props) => {
 
   console.log(movie);
 
-  return <div></div>;
+  return (
+    <div>
+      <div className="relative w-full h-[95vh] -z-10 ">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt="movie poster"
+          width={0}
+          height={0}
+          sizes="100vw"
+          fill
+          style={{
+            objectFit: 'cover',
+            objectPosition: '50% 20%',
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
