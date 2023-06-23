c77c37b61dcae1bec65e563d53c4fc2c17dca6df
Kadir <k1d12r04@gmail.com>
2023-06-23 18:46:34 +0300

    Finish responsive movie title and description

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -19,8 +19,8 @@ const Banner = ({ netflixOriginals }: Props) => {
   console.log(movie);
 
   return (
-    <div>
-      <div className="relative w-full h-[95vh] -z-10 ">
+    <div className="flex flex-col space-y-2 px-4 py-16 md:space-y-4 lg:h-[100vh] lg:justify-end">
+      <div className="absolute left-0 top-0 -z-10 h-[95vh] w-full ">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           alt="movie poster"
@@ -34,6 +34,13 @@ const Banner = ({ netflixOriginals }: Props) => {
           }}
         />
       </div>
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-sm text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl ">
+        {movie?.overview}
+      </p>
     </div>
   );
 };
